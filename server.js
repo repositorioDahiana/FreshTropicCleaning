a041417ac4f6138d42ed12556160ef743f302e5d
repositorioDahiana <brahiandiazladi@gmail.com>
2025-10-25 17:15:03 -0500

    Despliegue Backend

--- server.js
+++ server.js
@@ -39,4 +39,6 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-app.listen(3001, () => console.log("✅ Server running on port 3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
